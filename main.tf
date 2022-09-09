da0301e16f853b2cc49ea78f1269a0cb7175079d
Tiago Domingues <tiago@everonline.eu>
2022-09-10 00:22:42 +0100

    Changed cluster resource

--- main.tf
+++ main.tf
@@ -11,26 +11,17 @@ provider "linode" {
 }
 
 resource "linode_lke_cluster" "lke_cluster" {
-    label       = "lke.everonline.eu"
-    k8s_version = "1.23"
-    region      = "eu-west"
-    tags        = ["linode"]
+  label       = "lke.everonline.eu"
+  k8s_version = "1.23"
+  region      = "eu-west"
 
-    pool {
-        type  = "g6-standard-1"
-        count = 1
+  pool {
+    type  = "g6-standard-1"
+    count = 1
 
-        autoscaler {
-          min = 1
-          max = 2
-        }
+    autoscaler {
+      min = 1
+      max = 2
     }
-
-  # Prevent Count From Overriding Autoscaler
-
-  lifecycle {
-    ignore_changes = [
-      pool.0.count
-    ]
   }
 }
