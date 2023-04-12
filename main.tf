d18714db03df50297ae55ea809b5cbed08cdbeaf
Tiago Domingues <tiago@everonline.eu>
2023-04-12 22:39:13 +0100

    Adjust Configuration

--- main.tf
+++ main.tf
@@ -11,35 +11,35 @@ provider "linode" {
 }
 
 resource "linode_lke_cluster" "lke_cluster" {
-    label       = "lke.everonline.eu"
-    k8s_version = "1.25"
-    region      = "eu-west"
-    tags        = ["prod"]
+  label       = "lke.everonline.eu"
+  k8s_version = "1.25"
+  region      = "eu-west"
+  tags        = ["linode"]
 
-    pool {
-        type  = "g6-standard-1"
-        count = 1
+  pool {
+    type  = "g6-standard-1"
+    count = 1
 
-        autoscaler {
-          min = 1
-          max = 1
-        }
+    autoscaler {
+      min = 1
+      max = 1
     }
+  }
 
-    pool {
-        type  = "g6-standard-2"
-        count = 1
+  pool {
+    type  = "g6-standard-2"
+    count = 1
 
-        autoscaler {
-          min = 1
-          max = 1
-        }
+    autoscaler {
+      min = 1
+      max = 1
     }
+  }
 
   lifecycle {
     ignore_changes = [
       pool.0.count
     ]
   }
- 
+
 }
