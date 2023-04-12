9ec24f7eace4839ed5be609c0f584ede6d52a087
Tiago Domingues <tiago@everonline.eu>
2023-04-12 22:24:50 +0100

    Enable Auto Scaler

--- main.tf
+++ main.tf
@@ -19,12 +19,12 @@ resource "linode_lke_cluster" "lke_cluster" {
     pool {
         type  = "g6-standard-1"
         count = 1
-/*
+
         autoscaler {
           min = 1
-          max = 3
+          max = 1
         }
-*/        
+     
     }
 
 /*
