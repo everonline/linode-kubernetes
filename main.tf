4866d5423fc0610cda3e9d6677f31b982a30462e
Tiago Domingues <tiago@everonline.eu>
2023-04-12 22:15:59 +0100

    Disable Auto Scaler && Pool

--- main.tf
+++ main.tf
@@ -19,13 +19,15 @@ resource "linode_lke_cluster" "lke_cluster" {
     pool {
         type  = "g6-standard-1"
         count = 1
-
+/*
         autoscaler {
           min = 1
           max = 3
         }
+*/        
     }
 
+/*
     pool {
         type  = "g6-standard-2"
         count = 1
@@ -35,6 +37,7 @@ resource "linode_lke_cluster" "lke_cluster" {
           max = 3
         }
     }
+*/
 
   lifecycle {
     ignore_changes = [
