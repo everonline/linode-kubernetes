6e279a48dd34f55dae54a3549743269f09a66630
Tiago Domingues <tiago@everonline.eu>
2023-04-12 22:34:37 +0100

    Changed Configuration

--- main.tf
+++ main.tf
@@ -24,17 +24,15 @@ resource "linode_lke_cluster" "lke_cluster" {
           min = 1
           max = 1
         }
-     
     }
 
-/*
     pool {
         type  = "g6-standard-2"
         count = 1
 
         autoscaler {
           min = 1
-          max = 3
+          max = 1
         }
     }
 
@@ -43,5 +41,5 @@ resource "linode_lke_cluster" "lke_cluster" {
       pool.0.count
     ]
   }
-*/  
+ 
 }
