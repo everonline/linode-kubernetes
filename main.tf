253c60e4fd627314d0afca04a0e817f38c5a00e4
Tiago Domingues <tiago@everonline.eu>
2023-04-20 23:51:06 +0100

    Comment

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "linode_lke_cluster" "lke_cluster" {
 
 }
 
+/*
 provider "helm" {
   kubernetes { 
     config_path      = "kube-config"
@@ -83,4 +84,4 @@ resource "helm_release" "metrics_server" {
         value = "{--secure-port=8443}"
     }   
     
-}
+} */
