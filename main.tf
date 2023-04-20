a5312ff5f8685bae37007487ef2e4820c0498856
Tiago Domingues <tiago@everonline.eu>
2023-04-20 23:27:20 +0100

    Change Info

--- main.tf
+++ main.tf
@@ -48,6 +48,12 @@ resource "linode_lke_cluster" "lke_cluster" {
 
 }
 
+resource "local_file" "kubeconfig" {
+  depends_on = [linode_lke_cluster.lke_cluster]
+  filename   = "kube-config"
+  content    = base64decode(linode_lke_cluster.lke_cluster.kubeconfig)
+}
+
 provider "helm" {
   #kubernetes { 
   #  config_path      = "kube-config"
