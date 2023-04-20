1cf848e6e377f6f3737b0e98c5d1bf92a87b401c
Tiago Domingues <tiago@everonline.eu>
2023-04-20 13:12:35 +0100

    Add Secrets

--- main.tf
+++ main.tf
@@ -49,6 +49,9 @@ resource "linode_lke_cluster" "lke_cluster" {
 }
 
 provider "helm" {
+  kubernetes {
+    config_path = ${{ secrets.KUBE_CONFIG }}
+  }
 }
 
 resource "helm_release" "metrics_server" {
